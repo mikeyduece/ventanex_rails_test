3c444ab5fa80648789145cc7e5deba1b666074a3
Mike Heft <mikeheft@gmail.com>
2020-05-23 13:59:12 -0600

    add new/create action to pay accounts controller

--- app/controllers/pay_accounts_controller.rb
+++ app/controllers/pay_accounts_controller.rb
@@ -1,4 +1,5 @@
 class PayAccountsController < ApplicationController
+  helper_method :pay_account
 
   # TODO: Add lookup to Account class
 
@@ -6,7 +7,21 @@ class PayAccountsController < ApplicationController
     @pay_accounts = PayAccount.all
   end
 
+  def new
+    @pay_account = PayAccount.new
+  end
+
   def create
+  end
+
+  private
+
+  def pay_account
+    @pay_account ||= PayAccount.find_by(id: pay_account_params[:id])
+  end
+
+  def pay_account_params
+    params.require(:pay_account).permit(:id, :nickname, :account_type, :account_number)
   end
 
 end
