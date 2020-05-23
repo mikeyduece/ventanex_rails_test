756ca4fcf008d90d3f5a3858f3e7c1dd16809392
Mike Heft <mikeheft@gmail.com>
2020-05-23 14:38:44 -0600

    Add functionality to read xml file

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,4 @@
 class Account < ApplicationRecord
+
+  validates :loan_number, :borrower_name, presence: true
 end
--- app/services/account.rb
+++ app/services/account.rb
@@ -1,3 +1,29 @@
 class Account
-  # TODO Add code here
+
+  def loan_file
+    xml_file = File.read('app/assets/xml/loan.xml')
+    @loan_file ||= Nokogiri::XML(xml_file)
+  end
+
+  # Searches and displays borrow name
+  def borrow_name
+    node_text('BorrowerFullNameUnformatted')
+  end
+
+  # Searches and displays loan number
+  def loan_number
+    node_text('LoanNumber')
+  end
+
+  private
+
+  # Retrieves the inner text from the specified node
+  def node_text(node)
+    search_xml(node).children.text
+  end
+
+  # Searches the XML document for the relevant node
+  def search_xml(node)
+    loan_file.xpath("//#{node}")
+  end
 end
